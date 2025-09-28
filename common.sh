d9fe6fde33569ab2df47682d9836debc91c46df9
Manikanta Akula <maniakula202@gmail.com>
2025-09-28 14:13:13 +0530

    shell practice

--- common.sh
+++ common.sh
@@ -68,7 +68,7 @@ app_setup(){
 }
 
 systemd_setup(){
-    cp $PRESENT_DIRECTORY/$app_name.service /etc/systemd/system/$app_name.service &>>$LOG_FILE
+    cp $PRESENT_DIRECTORY/$app_name.service /etc/systemd/system/$app_name.service
     VALIDATE $? "Copying the $app_name service"
 
     systemctl daemon-reload &>>$LOG_FILE
