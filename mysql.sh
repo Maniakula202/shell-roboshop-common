f72b7a00ae9d1c3f8dee16511a3e6e595720afd3
Manikanta Akula <maniakula202@gmail.com>
2025-09-28 13:14:39 +0530

    shell practice

---
+++ mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_root
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing mysql"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling mysql"
+
+systemctl start mysqld  
+VALIDATE $? "Starting mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
+
---
+++ rabbitmq.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+check_root
+
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo  &>>$LOG_FILE
+VALIDATE $? "Copying the code from rabbitmq repo"
+
+dnf install rabbitmq-server -y  &>>$LOG_FILE
+VALIDATE $? "Installing rabbitmq"
+
+systemctl enable rabbitmq-server  &>>$LOG_FILE
+VALIDATE $? "Enabling rabbitmq"
+
+systemctl start rabbitmq-server  &>>$LOG_FILE
+VALIDATE $? "Starting rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123  &>>$LOG_FILE
+VALIDATE $? "Adding the user "
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$LOG_FILE
+VALIDATE $? "Setting up the permissions"
